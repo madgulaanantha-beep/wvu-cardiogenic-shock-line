a3f0ed9291d5c58557acd15afc0f3394ec9f46a7
Anantha Sriharsha Madgula <anantha.madgula@hsc.wvu.edu>
2026-09-06 14:38:26 +0000

    Rename app to WVU Cardiogenic Shock Line

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'shock-activate-v4';
+const CACHE = 'shock-activate-v5';
 const PRECACHE = ['./manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png', './icons/icon-180.png'];
 
 self.addEventListener('install', (e) => {
