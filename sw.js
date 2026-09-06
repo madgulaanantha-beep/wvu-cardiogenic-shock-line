dcf20878163d91c65fc995695038b52ed454d3af
Anantha Sriharsha Madgula <anantha.madgula@hsc.wvu.edu>
2026-09-06 14:33:45 +0000

    Drop AMI extras, QI/MRN; keep auto DTMF 1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'shock-activate-v2';
+const CACHE = 'shock-activate-v3';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png', './icons/icon-180.png'];
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
